7bec21e5094c0b8b79731deef6d92ac5d22f0923
Wesley José dos Santos Toledo <90799825+WesleyJSToledo@users.noreply.github.com>
2023-04-03 14:25:01 -0300

    create pieces and defensive programming

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,22 +3,30 @@ package application;
 import chess.ChessPiece;
 
 public class UI {
+
 	
 	public static void printBoard(ChessPiece[][] pieces) {
+		System.out.println("\t   A B C D E F G H ");
+		System.out.println();
+
 		for(int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + "  ");
+			System.out.print("\t" + (8 - i) + "  ");
 			for(int j = 0; j < pieces[i].length; j++) {
 				printPiece(pieces[i][j]);
 			}
+			System.out.print("  " + (8 - i));
+			
 			System.out.println();
 		}
 		System.out.println();
-		System.out.println("   A B C D E F G H");
+		System.out.println("\t   A B C D E F G H");
+
 	}
 	
 	private static void printPiece (ChessPiece piece) {
+		
 		if(piece == null) {
-			System.out.print("-");
+				System.out.print("-");
 		} else {
 			System.out.print(piece);
 		}
