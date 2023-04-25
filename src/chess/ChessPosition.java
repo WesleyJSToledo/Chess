d581313ab98b712e71e78ae51a2f8599f275b771
WesleyJSToledo <wesleyjstoledo@gmail.com>
2023-04-25 13:22:09 -0300

    Piece Move count

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -7,6 +7,9 @@ public class ChessPosition {
 	
 	private char column;
 	private int row;
+	
+	
+	
 	public ChessPosition(char column, int row) {
 		if(column < 'A' || column > 'H' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from A1 to H8.");
@@ -16,12 +19,16 @@ public class ChessPosition {
 		this.row = row;
 	}
 	
+	
+	
 	public char getColumn() {
 		return column;
 	}
 	public int getRow() {
 		return row;
 	}
+	
+	
 	
 	protected Position toPosition() {
 		return new Position((8 - row), (column - 'A'));
